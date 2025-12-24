189cd15b69d2de610dd3c3523da3898b897b52db
xaistrying <xaistrying@gmail.com>
2025-12-24 15:55:58 +0700

    feat: add initial infrastructure setup

---
+++ infrastructure/outputs.tf
@@ -0,0 +1,14 @@
+output "gateway_public_ip" {
+  description = "Public IP address of gateway"
+  value       = azurerm_public_ip.pip_gateway.ip_address
+}
+
+output "gateway_private_ip" {
+  description = "Private IP address of gateway"
+  value       = azurerm_network_interface.nic_gateway.private_ip_address
+}
+
+output "app_private_ip" {
+  description = "Private IP address of app"
+  value       = azurerm_network_interface.nic_app.private_ip_address
+}
