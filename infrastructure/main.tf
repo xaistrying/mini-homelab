2d1fb73c0e57f76432e3c75ec81f5cdc7432b301
xaistrying <xaistrying@gmail.com>
2026-01-06 02:02:58 +0700

    feat: allow inbound 80/443 for vm_gateway and 443 for vm_app via NSG

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -69,6 +69,30 @@ resource "azurerm_network_security_group" "nsg_gateway" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = local.common_tags
 }
 
@@ -164,6 +188,18 @@ resource "azurerm_network_security_group" "nsg_app" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
+    source_address_prefix      = "10.159.1.0/24"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTPSFromGateway"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
     source_address_prefix      = "10.159.1.0/24"
     destination_address_prefix = "*"
   }
